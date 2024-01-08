4cd9fbe1ee28cf90ba349590fbf9d57c04e6fe28
hyejung <lstarsl1214@naver.com>
2024-01-08 16:06:45 +0900

    잘 해보자

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: '화이팅'),
+      home: const MyHomePage(title: '잘 해보자!!'),
     );
   }
 }
