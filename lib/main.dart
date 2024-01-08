5352962abe5ca2b5b979f4c2ceff08c2690ac2ad
Sangin Han <hugedreamhd@gmail.com>
2024-01-08 15:43:44 +0900

    제발 되라

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHome(title: '최종 데모'),
+      home: const MyHome(title: '제발 되라'),
     );
   }
 }
